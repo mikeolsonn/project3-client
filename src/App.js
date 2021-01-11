cc23792e15a60c18d2920d6f75a7014a28672123
Mike Olson <mikeolsonn97@gmail.com>
2021-01-10 20:48:52 -0500

    imports hdr and ftr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
      <p>Project 3</p>
+     <Footer />
     </div>
   );
 }
