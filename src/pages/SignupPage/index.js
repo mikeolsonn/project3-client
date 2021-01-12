8aadff1ff6926ef054c09cf55603082bc0c422fd
Mike Olson <mikeolsonn97@gmail.com>
2021-01-11 21:30:57 -0500

    adds controlled component

--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -1,7 +1,61 @@
+import { useState } from 'react';
+
 function SignupPage(props) {
+    const [formState, setFormState] = useState(getInitialFormState);
+
+    function getInitialFormState() {
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+        }
+    }
+//helper functions for handling change and submits
+    function handleChange(event) {
+        setFormState(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }));
+    }
+
+    function handleSubmit(event) {
+         event.preventDefault(); // so page doesnt refresh and we dont lose state
+         console.log('submitted form data: ', formState);
+// TODO: make ajax request to sign user
+         setFormState(getInitialFormState);
+         props.history.push('/dashboard')
+    }
+
     return (
-        <div>
-            SignupPage
+        <div className="Page">
+            <form onSubmit={handleSubmit}>
+                <input 
+                value={formState.firstName} 
+                onChange={handleChange}
+                name="firstName" 
+                type="text"
+                 />
+                <input 
+                value={formState.lastName} 
+                onChange={handleChange}
+                name="lastName" 
+                type="text"
+                 />
+                <input 
+                value={formState.email} 
+                onChange={handleChange}
+                name="email" 
+                type="email"
+                 />
+                <input 
+                value={formState.password} 
+                onChange={handleChange}
+                name="password" 
+                type="password"
+                 />
+                 <button>Sign Up</button>
+            </form>
         </div>
     );
 }
