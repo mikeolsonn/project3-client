4e5bf01ba3f85a26a8c0341edaaac1a8dc857e10
Mike Olson <mikeolsonn97@gmail.com>
2021-01-11 19:27:40 -0500

    adds basic routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <Header />
-     <p>Project 3</p>
+        <main>
+          <Switch>
+            <Route exact path="/" render={props =>
+              <HomePage />
+            } />
+            <Route exact path="/dashboard" render={props =>
+              <DashboardPage />
+            } />
+            <Route exact path="/signup" render={props =>
+              <SignupPage />
+            } />
+            <Route exact path="/login" render={props =>
+              <LoginPage />
+            } />
+          </Switch>
+        </main>
      <Footer />
     </div>
   );
